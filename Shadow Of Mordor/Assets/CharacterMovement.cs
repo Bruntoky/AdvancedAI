ac29c3f7557409fa352bcba63d722e92dacc60d9
Bruntoky <bruntoky@sheridancollege.ca>
2021-10-13 20:25:12 -0400

    Added NavMesh
    Removed 2 enemies
    Added FSM to Enemy
    Added NavMesh to Level

--- Shadow Of Mordor/Assets/CharacterMovement.cs
+++ Shadow Of Mordor/Assets/CharacterMovement.cs
@@ -7,16 +7,16 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float speed = 7.5f;
-    public float jumpSpeed = 8.0f;
-    public float gravity = 20.0f;
-    public Transform playerCameraParent;
-    public float lookSpeed = 2.0f;
-    public float lookXLimit = 60.0f;
+    public float speed = 7.5f;// grounded movement speed
+    public float jumpSpeed = 8.0f;//movement speed while in the air
+    public float gravity = 20.0f;//speed at which you fall
+    public Transform playerCameraParent;//camera linked to player
+    public float lookSpeed = 2.0f;//speed the camera rotates
+    public float lookXLimit = 60.0f;//maximum angle you can look to your sides
 
     CharacterController characterController;
-    Vector3 moveDirection = Vector3.zero;
-    Vector2 rotation = Vector2.zero;
+    Vector3 moveDirection = Vector3.zero;//direction your moving
+    Vector2 rotation = Vector2.zero;//direction your looking
 
     [HideInInspector]
     public bool canMove = true;
@@ -24,23 +24,25 @@ public class CharacterMovement : MonoBehaviour
     void Start()
     {
         characterController = GetComponent<CharacterController>();
-        rotation.y = transform.eulerAngles.y;
+        rotation.y = transform.eulerAngles.y;//ensures facing angle is correct
     }
 
     void Update()
     {
         if (characterController.isGrounded)
         {
-            // We are grounded, so recalculate move direction based on axes
+            // If we are grounded, so recalculate move direction based on axes
             Vector3 forward = transform.TransformDirection(Vector3.forward);
             Vector3 right = transform.TransformDirection(Vector3.right);
+            //set move speed
             float curSpeedX = canMove ? speed * Input.GetAxis("Vertical") : 0;
             float curSpeedY = canMove ? speed * Input.GetAxis("Horizontal") : 0;
+            //sets movement direction
             moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
             if (Input.GetButton("Jump") && canMove)
             {
-                moveDirection.y = jumpSpeed;
+                moveDirection.y = jumpSpeed;//makes the character move up
             }
         }
 
@@ -49,7 +51,7 @@ public class CharacterMovement : MonoBehaviour
         // as an acceleration (ms^-2)
         moveDirection.y -= gravity * Time.deltaTime;
 
-        // Move the controller
+        // Move the character
         characterController.Move(moveDirection * Time.deltaTime);
 
         // Player and Camera rotation
